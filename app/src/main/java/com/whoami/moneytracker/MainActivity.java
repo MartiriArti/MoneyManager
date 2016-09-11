e8c9e13a9e1f9fff6a612b22737275b7cf66c9f3
WhoAmI <antonyo007h@mail.ru>
2016-09-11 22:28:10 +0300

    project with method OnStart and simple Log

--- app/src/main/java/com/whoami/moneytracker/MainActivity.java
+++ app/src/main/java/com/whoami/moneytracker/MainActivity.java
@@ -1,13 +1,29 @@
 package com.whoami.moneytracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = "Life cycle";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "onCreate()", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onCreate()");
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Toast.makeText(getApplicationContext(), "onStart()", Toast.LENGTH_SHORT).show();
+        Log.i(TAG, "onStart()");
+    }
+
 }

4b7d20245b2279bbe13a3aeeaf2651aeb191871d
WhoAmI <antonyo007h@mail.ru>
2016-09-15 15:25:21 +0300

    Сделаны поправки в работе и по оформлению, удалены лишние участки кода и файлы. Протестированно на реальном устройстве. Скриншоты прилагаются.

--- app/src/main/java/com/whoami/moneytracker/MainActivity.java
+++ app/src/main/java/com/whoami/moneytracker/MainActivity.java
@@ -2,22 +2,17 @@ package com.whoami.moneytracker;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.widget.Toast;
-import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -35,6 +30,7 @@ public class MainActivity extends AppCompatActivity
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                Toast.makeText(getApplicationContext(), "Клик...", Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -71,6 +67,7 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.action_settings) {
+            Toast.makeText(getApplicationContext(), "Настройки", Toast.LENGTH_SHORT).show();
             return true;
         }
 
@@ -83,15 +80,15 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_spend) {
-
+            Toast.makeText(getApplicationContext(), "Траты", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_category) {
-
+            Toast.makeText(getApplicationContext(), "Категории", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_statistics) {
-
+            Toast.makeText(getApplicationContext(), "Статистика", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_settings) {
-
+            Toast.makeText(getApplicationContext(), "Настройки", Toast.LENGTH_SHORT).show();
         } else if (id == R.id.nav_exit) {
-
+            Toast.makeText(getApplicationContext(), "Выход", Toast.LENGTH_SHORT).show();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
